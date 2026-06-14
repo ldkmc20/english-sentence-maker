46e88c0f5fef51844293e5576ca84a9d46e7b504
ldkmc <ldkmc@users.noreply.github.com>
2026-06-14 21:18:25 +0900

    Bust cached app assets

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 const cacheName = "ksej-v2";
-const assets = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css?v=20260614-2300",
+  "./app.js?v=20260614-2300",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
